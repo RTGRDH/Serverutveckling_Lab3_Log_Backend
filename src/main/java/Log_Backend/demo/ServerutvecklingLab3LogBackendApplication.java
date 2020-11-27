67ecfabc0daf4db0a6aedf1f82231ce790883681
Ernst Reutergårdh <kedre@kth.se>
2020-11-27 15:27:00 +0100

    -Init Commit

---
+++ src/main/java/Log_Backend/demo/ServerutvecklingLab3LogBackendApplication.java
@@ -0,0 +1,13 @@
+package Log_Backend.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerutvecklingLab3LogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerutvecklingLab3LogBackendApplication.class, args);
+	}
+
+}
